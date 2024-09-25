c9f6ccefc108828524081c2ad95f4dc56e287bed
robin <robin.noble@ynov.com>
2024-09-25 10:48:11 +0200

    css

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { functions.Index(w, r) })
 
 	//load the CSS, the JS and the images
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
 	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./img"))))
 	http.Handle("/script/", http.StripPrefix("/script/", http.FileServer(http.Dir("./script"))))
 
