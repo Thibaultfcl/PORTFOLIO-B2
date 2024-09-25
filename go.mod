ce849144150f01afce9001209bf8e50288fb52e9
ThibaultF <thibault.frescaline@ynov.com>
2024-09-25 10:05:05 +0200

    server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module portfolio
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// port of the server
+const port = ":8080"
+
+func main() {
+	//load the CSS, the JS and the images
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./img"))))
+	http.Handle("/script/", http.StripPrefix("/script/", http.FileServer(http.Dir("./script"))))
+
+	//start the local host
+	fmt.Println("\n(http://localhost:8080/home) - Server started on port", port)
+	http.ListenAndServe(port, nil)
+}
