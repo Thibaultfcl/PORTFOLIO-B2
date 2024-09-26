6c261ee5283377f09c24c11252969af87bcb3f0b
robin <robin.noble@ynov.com>
2024-09-26 09:30:17 +0200

    html

--- functions/index.go
+++ functions/index.go
@@ -5,6 +5,6 @@ import "net/http"
 func Index(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./tmpl/index.html")
 }
-func Nav(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./tmpl/nav.html")
+func Projet(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./tmpl/projet.html")
 }
